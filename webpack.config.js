eebcfd94eb88fd95bfd3463f2abc78e1fec40b66
Adrian Hazelnut <hazelnutgh@gmail.com>
2018-10-28 22:33:58 +0000

    Update File structure

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+const cleanP = require('clean-webpack-plugin');
+const cssPlugin = require('mini-css-extract-plugin');
+
+module.export = {
+    entry: {
+        name: "./src/js/main.js"
+    },
+    output: {
+        filename: "[name]_app.js",
+        path: path.resolve('dist')
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    cssPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "node-sass",
+                        options: {
+                            outputStyle: "compressed"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: "true"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg)/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "/imgz"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        cleanP(['dis']),
+        new htmlPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new cssPlugin({
+            filename: "[name].css",
+            chunck: "[id].css"
+        })
+    ]
+}
